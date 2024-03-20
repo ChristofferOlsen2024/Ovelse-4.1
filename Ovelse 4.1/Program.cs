58ccbc331817966792784b57904f5409ef705184
Christoffer Olsen <chris70944@gmail.com>
2024-03-20 21:52:03 +0100

    Add project files.

---
+++ Ovelse 4.1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Skriv dit navn.");
+string navn = Console.ReadLine();
+if (navn == "Christoffer")
+{
+    Console.WriteLine("Sikke et flot navn du har!");
+}
